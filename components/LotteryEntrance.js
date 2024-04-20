17e39151040ebcd03483c2e770323dcd69b681c6
Vivek <vivek.mitra@outlook.com>
2024-04-20 16:19:18 +1000

    exported the constants and updated lottery entrance function

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -1,13 +1,36 @@
-import { useWeb3Contract } from "react-moralis"
+import { useMoralis, useWeb3Contract } from "react-moralis"
+import { abi, contractAddresses } from "../constants"
+import { useEffect } from "react"
 
 export default function LotteryEntrance() {
-    /*    const { runContractFunction: enterRaffle } = useWeb3Contract({
-        abi: x, //,
-        contractAddress: {},
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    const chainId = parseInt(chainIdHex)
+    const raffleAddress = chainId in contractAddresses ? contractAddresses[chainId][0] : null
+    const { runContractFunction: enterRaffle } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
         functionName: {},
         params: {},
         msgValue: {},
-    }) */
+    })
+
+    const { runContractFunction: getEntranceFee } = useWeb3Contract({
+        abi: abi,
+        contractAddress: raffleAddress,
+        functionName: "getEntranceFee",
+        params: {},
+    })
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            //try to read the raffle entrance fee
+            async function updateUI() {
+                const entranceFee = await getEntranceFee()
+                console.log(entranceFee)
+            }
+            updateUI()
+        }
+    }, [isWeb3Enabled])
 
     return <div>Hi from lottery entrance</div>
 }
